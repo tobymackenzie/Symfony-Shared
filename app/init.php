6a0919b02978b0d668631c9e0db72e66f692740d
Toby Mackenzie <tobym@tobymackenzie.com>
2012-05-18 23:23:47 -0400

    app/init: changed 'isSimpleCLI' config parameter to 'loadSymfony' (inverse values) 	default for 'loadSymfony' *BCBREAK

--- app/init.php
+++ app/init.php
@@ -2,6 +2,8 @@
 if(!isset($tjmGlobals)) $tjmGlobals = Array();
 if(!array_key_exists('isCli', $tjmGlobals))
 	$tjmGlobals['isCli'] = (php_sapi_name() == 'cli');
+if(!array_key_exists('loadSymfony', $tjmGlobals))
+	$tjmGlobals['loadSymfony'] = true;
 if(!array_key_exists('pathApp', $tjmGlobals)){
 	$tjmGlobals['pathApp'] = ($tjmGlobals['isCli'])
 		? exec('pwd').'/app'
@@ -15,10 +17,7 @@ if(!array_key_exists('pathVendor', $tjmGlobals)){
 	$tjmGlobals['pathVendor'] = $tjmGlobals['pathApp']."/../vendor";
 }
 
-if(!(
-	array_key_exists('isSimpleCLI', $tjmGlobals)
-	&& $tjmGlobals['isSimpleCLI']
-)){
+if($tjmGlobals['loadSymfony']){
 	require_once $tjmGlobals['pathApp'].'/bootstrap.php.cache';
 	require_once $tjmGlobals['pathApp'].'/AppKernel.php';
 }
