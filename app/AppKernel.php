1f8d4601cbcda1c53267207863f0a4aeaf6d8b74
Toby Mackenzie <tobym@tmacbook.neo.rr.com>
2013-03-05 02:04:49 -0500

    updated to work with Symfony 2.1 or to load the 2.1 Standard items. Removed some items that will now be placed in Symfony Initial due to them being more useful before running composer or just being better there

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -1,37 +1,77 @@
 <?php
 namespace TJM\Shared\app;
 
-use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Kernel;
 
 class AppKernel extends Kernel{
-	protected $pathApp = null;
-
+	/*=====
+	==initialization
+	=====*/
 	public function registerBundles(){
 		$bundles = array(
-			new \Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
-			new \Symfony\Bundle\SecurityBundle\SecurityBundle(),
-			new \Symfony\Bundle\TwigBundle\TwigBundle(),
-			new \Symfony\Bundle\MonologBundle\MonologBundle(),
-			new \Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
-			new \Symfony\Bundle\DoctrineBundle\DoctrineBundle(),
-			new \Symfony\Bundle\AsseticBundle\AsseticBundle(),
-			new \Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-			new \JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+			//--standard
+			//---framework
+			new \Symfony\Bundle\FrameworkBundle\FrameworkBundle()
+			//---standard symfony
+			,new \Symfony\Bundle\AsseticBundle\AsseticBundle()
+			,new \Symfony\Bundle\MonologBundle\MonologBundle()
+			,new \Symfony\Bundle\SecurityBundle\SecurityBundle()
+			,new \Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle()
+			,new \Symfony\Bundle\TwigBundle\TwigBundle()
+			//---sensio extra
+			,new \Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle()
+			//---JMS extras
+            ,new \JMS\AopBundle\JMSAopBundle()
+            ,new \JMS\DiExtraBundle\JMSDiExtraBundle($this)
+			,new \JMS\SecurityExtraBundle\JMSSecurityExtraBundle()
+			//---doctrine
+			,new \Doctrine\Bundle\DoctrineBundle\DoctrineBundle()
 		);
 
 		if (in_array($this->getEnvironment(), array('dev', 'test'))) {
 			$bundles[] = new \Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
-			$bundles[] = new \Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
 			$bundles[] = new \Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+			$bundles[] = new \Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
 		}
 
 		return $bundles;
 	}
 
 	public function registerContainerConfiguration(LoaderInterface $loader){
-		$pathApp = ($this->pathApp) ? $this->pathApp : __DIR__.'/../../../../../app';
-		$loader->load($pathApp.'/config/config_'.$this->getEnvironment().'.yml');
+		$appPath = (isset($GLOBALS['tjmGlobals']) && isset($GLOBALS['tjmGlobals']['paths']) && isset($GLOBALS['tjmGlobals']['paths']['app']))
+			? $GLOBALS['tjmGlobals']['paths']['app']
+			: __DIR__.'/../../../../../app'
+		;
+		$loader->load($appPath.'/config/config_'.$this->getEnvironment().'.yml');
+	}
+
+	/*=====
+	==process requests
+	=====*/
+	/*
+	Method: processRequest
+	Process a request in the standard edition fashion.
+	Parameters:
+		options(Map):
+			cache(Boolean): whether to use an AppCache
+			request(Request): specify an alternative request to process
+	*/
+	public function processRequest($options = Array()){
+		$kernel = $this;
+		$kernel->loadClassCache();
+		if(isset($options['cache']) && $options['cache'] === true){
+			//-# untested
+			$kernel = new \AppCache($kernel);
+		}
+		if(isset($options['request'])){
+			$request = $options['request'];
+		}else{
+			$request = Request::createFromGlobals();
+		}
+		$response = $kernel->handle($request);
+		$response->send();
+		$kernel->terminate($request, $response);
 	}
 }
-
--- app/init.php
+++ app/init.php
@@ -1,23 +1,25 @@
 <?php
 if(!isset($tjmGlobals)) $tjmGlobals = Array();
-if(!array_key_exists('isCli', $tjmGlobals))
+if(!isset($tjmGlobals['paths'])) $tjmGlobals['paths'] = Array();
+if(!isset($tjmGlobals['isCli']))
 	$tjmGlobals['isCli'] = (php_sapi_name() == 'cli');
-if(!array_key_exists('loadSymfony', $tjmGlobals))
-	$tjmGlobals['loadSymfony'] = true;
-if(!array_key_exists('pathApp', $tjmGlobals)){
-	$tjmGlobals['pathApp'] = ($tjmGlobals['isCli'])
+// if(!isset($tjmGlobals['loadSymfony']))
+// 	$tjmGlobals['loadSymfony'] = false;
+if(!isset($tjmGlobals['paths']['app'])){
+	$tjmGlobals['paths']['app'] = ($tjmGlobals['isCli'])
 		? exec('pwd').'/app'
 		: $_SERVER['DOCUMENT_ROOT'].'/../app'
 	;
 }
-if(!array_key_exists('pathSrc', $tjmGlobals)){
-	$tjmGlobals['pathSrc'] = $tjmGlobals['pathApp']."/../src";
+if(!isset($tjmGlobals['paths']['src'])){
+	$tjmGlobals['paths']['src'] = $tjmGlobals['paths']['app']."/../src";
 }
-if(!array_key_exists('pathVendor', $tjmGlobals)){
-	$tjmGlobals['pathVendor'] = $tjmGlobals['pathApp']."/../vendor";
+if(!isset($tjmGlobals['paths']['vendor'])){
+	$tjmGlobals['paths']['vendor'] = $tjmGlobals['paths']['app']."/../vendor";
 }
+$tjmGlobals['paths']['shared'] = __DIR__ . '/..';
 
-if($tjmGlobals['loadSymfony']){
-	require_once $tjmGlobals['pathApp'].'/bootstrap.php.cache';
-	require_once $tjmGlobals['pathApp'].'/AppKernel.php';
-}
+// if($tjmGlobals['loadSymfony']){
+// 	require_once $tjmGlobals['paths']['app'].'/bootstrap.php.cache';
+// 	require_once $tjmGlobals['paths']['app'].'/AppKernel.php';
+// }
